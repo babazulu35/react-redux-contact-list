a7d85ceec9ddd093499e793b25831591ee50839d
DESKTOP-P9H07VC\hakan <hakan@brewww.com>
2017-11-20 18:01:49 +0300

    Backend service,react, redux components

---
+++ src/actions/contact-actions.js
@@ -0,0 +1,30 @@
+import { contacts } from '../contact-data';
+import { client } from './index';
+
+const url = '/contacts';
+
+export function fetchContacts() {
+    return dispatch => {
+        dispatch({
+            type: 'FETCH_CONTACTS',
+            payload: client.get(url)
+        })
+    }
+}
+
+export function newContact() {
+    return dispatch => {
+        dispatch({
+            type: 'NEW_CONTACT'
+        })
+    }
+}
+
+export function saveContact(contact) {
+    return dispatch => {
+        return dispatch({
+            type: 'SAVE_CONTACT',
+            payload: client.post(url, contact)
+        })
+    }
+}
---
+++ src/contact-data.js
@@ -0,0 +1,19 @@
+export const contacts = [{
+        _id: "1",
+        name: {
+            first: "Hakan",
+            last: "Huriyet"
+        },
+        phone: "555",
+        email: "john@gmail.com"
+    },
+    {
+        _id: "2",
+        name: {
+            first: "Ella",
+            last: "Wella"
+        },
+        phone: "777",
+        email: "ellawella@gmail.com"
+    }
+]
